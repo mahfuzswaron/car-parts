a9a874166b9c65f3bb4b23acd3644b2bb1c74c61
mahfuzswaron <mahfuzswaron@gmail.com>
2022-05-22 23:31:42 +0600

    business summery added

---
+++ src/components/pages/HomePage/BusinessSummery.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const BusinessSummery = () => {
+    return (
+        <div className='flex justify-center p-5 w-full scale-125 '>
+            <div class="stats shadow">
+
+                <div class="stat ">
+                    <div class="stat-figure text-secondary">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4" />
+                        </svg>
+                    </div>
+                    <div class="stat-title">Orders</div>
+                    <div class="stat-value">31K</div>
+                    <div class="stat-desc">Jan 1st - Feb 1st</div>
+                </div>
+
+                <div class="stat ">
+                    <div class="stat-figure text-secondary">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
+                        </svg>
+                    </div>
+                    <div class="stat-title">New Users</div>
+                    <div class="stat-value">4,200</div>
+                    <div class="stat-desc">↗︎ 400 (22%)</div>
+                </div>
+
+                <div class="stat ">
+                    <div class="stat-figure text-secondary">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
+                        </svg>
+                    </div>
+                    <div class="stat-title">New Registers</div>
+                    <div class="stat-value">1,200</div>
+                    <div class="stat-desc">↘︎ 90 (14%)</div>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default BusinessSummery;
