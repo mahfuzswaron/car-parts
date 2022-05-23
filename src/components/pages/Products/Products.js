9b19be32d97dfd5ab30bbe75f9206658c50ca788
mahfuzswaron <mahfuzswaron@gmail.com>
2022-05-23 23:05:22 +0600

    code optimized

--- src/components/pages/Products/Products.js
+++ src/components/pages/Products/Products.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import useProducts from '../../../hooks/useProducts';
+import ProductCard from './ProductCard';
 
 const Products = () => {
+    const [products] = useProducts([]);
+    const [items, setItems] = useState(3)
+    const slicedProducts = products.slice(0, items);
     return (
-        <div>
-            <h1>this is products</h1>
-        </div>
+        <section>
+            <div className='mb-10'>
+                <h3 className='text-4xl text-center text-primary font-semibold mb-10 mt-5'>Products</h3>
+                <div className='flex justify-center mx-auto'>
+                    <div className='grid grid-cols-3 gap-3'>
+                        {
+                            slicedProducts.map(product => <ProductCard
+                                key={product._id}
+                                product={product}
+                            ></ProductCard>)
+                        }
+                    </div>
+                </div>
+            </div>
+            {
+                items === 3 &&
+                <div className='flex justify-center mb-3'>
+                        <button onClick={() => setItems(9)} className='btn btn-primary rounded-xl'>See all</button>
+                    </div>
+            }
+        </section>
     );
 };
 
