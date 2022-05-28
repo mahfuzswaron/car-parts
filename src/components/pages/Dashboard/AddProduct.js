0214c5145372158da5c10bbc5ef0d8f4b2421255
mahfuzswaron <mahfuzswaron@gmail.com>
2022-05-28 20:50:27 +0600

    add product done

--- src/components/pages/Dashboard/AddProduct.js
+++ src/components/pages/Dashboard/AddProduct.js
@@ -1,9 +1,55 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const AddProduct = () => {
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    function onSubmit(data) {
+        fetch('http://localhost:5000/addpart', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }
     return (
         <div>
-            <h1>Add Product</h1>
+            <h3 className='text-4xl text-primary text-center font-semibold mb-5'>Add Product</h3>
+            <form className='p-5 mb-5 mt-5 rounded-md grid grid-cols-1 gap-3' onSubmit={handleSubmit(onSubmit)}>
+
+                <input type='text' placeholder='Product' className='input input-bordered w-full ' {...register("name", {
+                    required: true
+                })} />
+                <p className='text-error'>{errors.name?.type === 'required' && "Name is required"}</p>
+
+                <input type='text' placeholder='Short Description' className='input input-bordered w-full ' {...register("description", {
+                    required: true
+                })} />
+                <p className='text-error'>{errors.description?.type === 'required' && "Description is required"}</p>
+
+                <input type='text' placeholder='Image url' className='input input-bordered w-full ' {...register("img", {
+                    required: true
+                })} />
+                <p className='text-error'>{errors.img?.type === 'required' && "Name is required"}</p>
+
+                <input type='number' placeholder='Available Quantity' className='input input-bordered w-full ' {...register("quantity", {
+                    required: true
+                })} />
+                <p className='text-error'>{errors.quantity?.type === 'required' && "Name is required"}</p>
+
+                <input type='number' placeholder='Minimum Order Quantity' className='input input-bordered w-full ' {...register("min_quantity", {
+                    required: true
+                })} />
+                <p className='text-error'>{errors.min_quantity?.type === 'required' && "Name is required"}</p>
+
+                <input type='number' placeholder='Price per unit' className='input input-bordered w-full ' {...register("price", {
+                    required: true
+                })} />
+                <p className='text-error'>{errors.price?.type === 'required' && "Name is required"}</p>
+                <input className='btn btn-primary' value='Add' type="submit" />
+            </form>
         </div>
     );
 };
--- src/components/shared/RequireAdmin.js
+++ src/components/shared/RequireAdmin.js
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { set } from 'react-hook-form';
-import { Navigate, useLocation } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import auth from '../../firebase.init';
 
 const RequireAdmin = ({ children }) => {
-    const [user, loading, error] = useAuthState(auth);
+    const [user, loading] = useAuthState(auth);
     const [fetchedUser, setFetchedUser] = useState({});
     useEffect(() => {
         fetch(`http://localhost:5000/users/${user.email}`)
@@ -17,15 +16,13 @@ const RequireAdmin = ({ children }) => {
     }, [user])
 
     if (!fetchedUser?.role || loading) {
-        console.log('loading');
         return <p>Loading...</p>
     }
     if (fetchedUser.role !== 'admin') {
-        console.log(fetchedUser);
         return <Navigate to='/dashboard' />
     }
 
-    return children
+    return children;
 };
 
 export default RequireAdmin;
