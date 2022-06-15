3e64c0717b8b6e1b63e3812ed0975349ecc4093c
mahfuzswaron <mahfuzswaron@gmail.com>
2022-06-15 16:37:57 +0600

    banner responsive done

--- src/components/pages/HomePage/Banner.js
+++ src/components/pages/HomePage/Banner.js
@@ -8,12 +8,16 @@ const Banner = () => {
                 className='w-full mx-auto relative '
                 src='https://i.ibb.co/BngB2v3/croped-banner-car.jpg' alt='' />
 
-            <div className='z-1 absolute ml-5'>
-                <h1 className='text-7xl text-white font-semibold'>Welcome to <br></br><span className='text-8xl text-primary '>CAR PARTS</span> </h1>
-                <p className='text-xl text-white my-3'>
-                    Get All the things you need here.
+            <div className='z-1 absolute ml-5 lg:pl-20 text-secondary'>
+                <h1 className='lg:text-7xl text-lg  font-semibold'>Welcome to <br></br><span className='lg:text-8xl text-xl text-primary lg:-ml-1'>CAR PARTS</span> </h1>
+                <p className='lg:text-xl text-xs lg:my-3 my-1 font-p w-1/2'>
+                    {
+                        `Manufacturing car? Don't thing about parts. 
+                        You are in the Parts Universe
+                        `
+                    }
                 </p>
-                <Link className='btn hover:btn-primary rounded-lg text-black bg-white p-3 my-3 border-none' to='/products'>BUY NOW</Link>
+                <Link className='btn btn-secondary rounded-full lg:text-xl text-xs btn-sm lg:btn-lg lg:px-10 lg:my-3 my-1 border-none' to='/products'>BUY NOW</Link>
             </div>
         </div>
     );
